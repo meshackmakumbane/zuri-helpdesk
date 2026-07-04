85f16e1466ae08295472b38c079c040e90684164
Meshack Makumbane <meshackmakumbane@gmai.com>
2026-07-04 08:24:46 +0200

    Implement database

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -1,16 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        const connection = await mongoose.connect(process.env.DB_URL);
+   try {
+    const connection = await mongoose.connect(process.env.DB_URL);
 
         console.log(`MongoDB Connected: ${connection.connection.host}`);
     } catch (error) {
-        console.error("Failed to connect to MongoDB");
-        console.error(error.message);
-
+        console.error(error); 
         process.exit(1);
     }
-};
-
+}
 export default connectDB;
